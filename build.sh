b432211a842b6656175cab61b7ff093394d60116
Leo Tietz <leo.tietz@gmail.com>
2023-03-20 17:44:50 +0100

    Switch to building postgres from source

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
-curl https://raw.githubusercontent.com/oufm/packelf/master/packelf.sh > packelf.sh && chmod +x packelf.sh
-
-sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-sudo apt-get update
-sudo apt-get install -y postgresql-client-15
-
-./packelf.sh /usr/lib/postgresql/15/bin/psql psql-static
+curl https://ftp.postgresql.org/pub/source/v15.2/postgresql-15.2.tar.gz | tar xzf -
+cd postgresql-15.2/
+./configure --without-readline --without-zlib LDFLAGS='--static'
+make -C src/bin/psql/ -ik
+cp src/bin/psql/psql ../psql-static
