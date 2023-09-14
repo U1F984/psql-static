61c35e4139d01f82f0ca972af78145336edf076f
Leo Tietz <leo.tietz@gmail.com>
2023-09-14 15:30:38 +0200

    Update to Postgres 16.0

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-curl https://ftp.postgresql.org/pub/source/v15.2/postgresql-15.2.tar.gz | tar xzf -
-cd postgresql-15.2/
+curl https://ftp.postgresql.org/pub/source/v16.0/postgresql-16.0.tar.gz | tar xzf -
+cd postgresql-16.0/
 ./configure --without-readline --without-zlib LDFLAGS='--static'
 make -C src/bin/psql/ -ik
 cp src/bin/psql/psql ../psql-static
